8983b4d09c518b475394c443d265c89999bbff3d
Flaming Penguin <tsc221645@uvg.edu.gt>
2023-06-08 12:51:24 -0600

    title

--- downloadmp3v2.py
+++ downloadmp3v2.py
@@ -9,15 +9,31 @@ from pytube import YouTube
 import os
 from tkinter import *
 
-
 #main window -----------------------------------------------
 main_window = Tk(className = "mp3downloader")
 main_window.geometry("1180x900")
 
 #background -------------------------------------------------
-cRect1 = Canvas(width=1180, height=900)
-cRect1.create_rectangle(1180,900,0,0, outline='white', fill="#535353", width='1')
+cRect1 = Canvas(width=1180, height=800)
+cRect1.create_rectangle(1180,800,0,0, outline='#272727', fill="#272727", width='1')
 cRect1.place(x=0, y=0)
+
+cRect2 = Canvas(width=1080, height=700)
+cRect2.create_rectangle(1080,700,0,0, outline='#535353', fill="#535353", width = '1')
+cRect2.place(x=50, y=50)
+
+#shapes -----------------------------------------------------
+
+#buttons ----------------------------------------------------
+
+#inputs ------------------------------------------------------
+
+#labels -----------------------------------------------------
+title = Label(main_window, text = "YouTube to MP3 Downloader", fg = 'white', bg= "#535353", font=("Gotham", 20))
+title.place(x=50, y=50)
+
+main_window.mainloop()
+
 def download():
     yt = YouTube(input('Enter the Url of the video you want to download: '))
     video = yt.streams.filter(only_audio=True).first()
