5df3903ec03d5e58b6756fd0d1d26c65c426da27
Flaming Penguin <tsc221645@uvg.edu.gt>
2023-06-08 13:07:29 -0600

    new font

--- downloadmp3v2.py
+++ downloadmp3v2.py
@@ -15,22 +15,25 @@ main_window.geometry("1180x900")
 
 #background -------------------------------------------------
 cRect1 = Canvas(width=1180, height=800)
-cRect1.create_rectangle(1180,800,0,0, outline='#272727', fill="#272727", width='1')
+cRect1.create_rectangle(1180,800,0,0, outline='#272727', fill="#272727")
 cRect1.place(x=0, y=0)
 
-cRect2 = Canvas(width=1080, height=700)
-cRect2.create_rectangle(1080,700,0,0, outline='#535353', fill="#535353", width = '1')
-cRect2.place(x=50, y=50)
-
 #shapes -----------------------------------------------------
+cRect2 = Canvas(width=400, height=700)
+cRect2.create_rectangle(1080,700,0,0, outline='#535353', fill="#535353", width=20 )
+cRect2.place(x=50, y=30)
+
+cRect3 = Canvas(width=650, height=700)
+cRect3.create_rectangle(1080,700,0,0, outline='#535353', fill="#535353", width=20 )
+cRect3.place(x=480, y=30)
 
 #buttons ----------------------------------------------------
 
 #inputs ------------------------------------------------------
 
 #labels -----------------------------------------------------
-title = Label(main_window, text = "YouTube to MP3 Downloader", fg = 'white', bg= "#535353", font=("Gotham", 20))
-title.place(x=50, y=50)
+title = Label(main_window, text = "YouTube to MP3 Downloader", fg = 'white', bg= "#535353", font=("System", 20))
+title.place(x=60, y=50)
 
 main_window.mainloop()
 
